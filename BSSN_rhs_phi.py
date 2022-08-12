57caa502c181c27a0d6324b8568e82183138d522
Natey <natey@Nateys-MacBook-Pro.local>
2022-08-12 19:33:46 +0200

    Name change in Ricci tensor function

--- BSSN_rhs_phi.py
+++ BSSN_rhs_phi.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tensor_algebra import * 
 
 partial_betaU_trace = einsum('ii', partial(betaU))
 
@@ -9,10 +10,11 @@ def phi_rhs(alpha, betaU, phi, K, partial_betaU_trace):
     dtdphi = - 1/6 * alpha * K + einsum('i,i', betaU, deriv_phi) + 1/6 * partial_betaU_trace
     return dtdphi
 
-def R_ij_phi(phi, tildegDD_ij):
+def R_ij_phi(phi, bar_gamma_ij):
     """
-    Phi part of traceless Ricci tensor decomposition
+    phi part of traceless Ricci tensor decomposition
     """
+    partial_phi = partial(phi)
     covphiDD_ij =  
     covphiUD_kk =
-    return -2 * covphiDD_ij - 2 * tildegDD_ij * covphiUD_kk + 4 * covaraint(phi) * covariant(phi) - 4 * tildegDD_ij * covariant(phi) * covariant(phi)   
+    return -2 * covphiDD_ij - 2 * bar_gamma_ij * covphiUD_kk + 4 * covaraint(phi) * covariant(phi) - 4 * bar_gamma_ij * covariant(phi) * covariant(phi)   

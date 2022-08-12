d0893e62c9f220ce111d88d6cbd09be3ab959b58
Hengrui Zhu <hz0693@princeton.edu>
2022-08-12 13:55:14 -0400

    K equation

--- BSSN_rhs.py
+++ BSSN_rhs.py
@@ -1,11 +1,18 @@
-twothirds = 2.0/3.0
+import numpy as np
+from tensor_algebra import *
 
-def dt_K(gammaUU_ij, D, alpha, tildeA_ij, K, rho, S, beta):
+twothirds = 2.0/3.0
+onethird = 1.0/3
+
+def dt_K(gammaDD_ij,gammaUU_ij, D, alpha, tildeADD_ij, K, rho, S, betaU_i):
     '''
     Evaluates rhs of Eq. 11.52 to evolve K
     '''
-    dt_K = -alpha*np.einsum('ij','j','i',gammaUU_ij,) + alpha*(einsum('ij','ij',tildeADD_ij,tildeAUU_ij))
-    
+    christoffelsymbolUDD = ChristoffelSymbolSecondKindUDD(gammaDD_ij,dx)
+    DDalphaDD_ij = covariant_derivative_covector(partial(alpha),christoffelsymbolUDD)
+    tildeAUU_ij = raise_tensor_index(tildeADD_ij, gammaUU_ij)
+    dt_K = -np.einsum('ij,ij',gammaUU_ij,DDalphaDD_ij) + alpha*(np.einsum('ij,ij',tildeADD_ij,tildeAUU_ij)+onethird*K**2) + np.einsum("i,i",betaU_i,partial(K))
+    return (dt_K)
 
 def barGamma(N,alpha, tildeADD_ij, betaU_i, barGammaDD_ij, ):
     '''

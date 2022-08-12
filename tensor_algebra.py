93a6e554d6b64acc4c6e0ae16eae0c1bad2a1d05
Hengrui Zhu <hz0693@princeton.edu>
2022-08-12 12:10:25 -0400

    partials

--- tensor_algebra.py
+++ tensor_algebra.py
@@ -1,13 +1,28 @@
 import numpy as np
 
-def partial(f,n):
-    df_n = f
-    return(df_n)
+def partial(f,dx):
+    df_i = (np.roll(f,-1,axis=0)-np.roll(f,1,axis=0))/2/dx
+    df_j = (np.roll(f,-1,axis=1)-np.roll(f,1,axis=1))/2/dx
+    df_k = (np.roll(f,-1,axis=2)-np.roll(f,1,axis=2))/2/dx
 
-def raise_index(fUD,gamma):
-    fUU=fUD
-    return(fUU)
+    df = np.stack([df_i,df_j,df_k],axis=0)
+    return(df)
+
+def partial_vector(fU,dx):
+    dfU_i = partial(fU[0],dx)
+    dfU_j = partial(fU[1],dx)
+    dfU_k = partial(fU[2],dx)
+    dfU = np.stack([dfU_i,dfU_j,dfU_k],axis=0)
+    return(dfU)
+
+
+def raise_vector_index(fD,gammaDD):
+    fU=fD
+    return(fU)
 
 def lower_index(fUU,gamma):
     fUD=fUU
     return(fUD)
+
+#def 
+#np.einsum('ij,ij',KUU,KDD)

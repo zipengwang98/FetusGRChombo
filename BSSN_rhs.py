e644130e3453b1188550a6204655a7c6d185ba97
Natey <natey@Nateys-MacBook-Pro.local>
2022-08-12 21:01:45 +0200

    added conformal ricci tensor

--- BSSN_rhs.py
+++ BSSN_rhs.py
@@ -47,3 +47,12 @@ def BSSN_RHS(Huge_list, t):
 
     out = np.zeros_like(Huge_list)
     return out
+
+def conformal_ricci():
+    bar_ricci = - 0.5 * np.einsum('lm ,lmij->ij', barGammaUU, partial_tensor(partial_tensor(barGammaDD, dx), dx)) + \
+                0.5 * (np.einsum('ki, jk->ij', barGammaDD, partial_vector(barChristoffelU, dx) + np.einsum('kj,ik->ij', barGammaDD, partial_ vector(barChristoffelU, dx))) + \
+                0.5 * (einsum('k,ijk->ij', barChristoffelU, ChristoffelSymbolFirstKindDDD) + einsum('k,jik->ij', barChristoffelU, ChristoffelSymbolFirstKinDDD)) + \
+                0.5 * ('lm, kli, jkm->ij', barGammaUU, ChristoffelSymbolSecondKindUDD, ChristoffelSymbolFirstKindDDD) + ('lm, klj, ikm->ij', barGammaUU, \ 
+                    ChristoffelSymbolSecondKindUDD, ChristoffelSymbolFirstKindDDD) + \
+                0.5 * (einsum('ki,jk->ij', barGammaDD, partial_vector(barChristoffelU, dx)) + einsum('kj,ik->ij', barGammaDD, partial_vector(barChristoffelU, dx)))
+    return bar_ricci

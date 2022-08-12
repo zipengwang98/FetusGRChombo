fca4e91a3850e8366f147dce73f2b844955fc9f6
scientistwang <scientistwang9877@gmail.com>
2022-08-12 13:24:12 -0400

    lower index functions

--- tensor_algebra.py
+++ tensor_algebra.py
@@ -36,13 +36,21 @@ def ChristoffelSymbolFirstKindDDD(gammaDD,dx):
     christoffelsymbolDDD = 0.5*(-dgammaDDD+np.transpose(dgammaDDD,(2,0,1,3))+np.transpose(dgammaDDD,(1,2,0,3)))
     return(christoffelsymbolDDD)
 
-def raise_vector_index(fD,gammaDD):
-    fU=fD
-    return(fU)
+def raise_vector_index(fD, gammaUU):
+    '''gammaUU is the inverse of the physical metric!'''
+    vector_out = np.array([0,0,0])
+    for i in range(3):
+        for j in range(3):
+            vector_out[i] += gammaUU[i][j] * fD[j]
+    return vector_out 
 
-def lower_index(fUU,gamma):
-    fUD=fUU
-    return(fUD)
+def lower_vector_index(fU, gammaDD):
+    '''gammaDD is the physical metric!!'''
+    vector_out = np.array([0,0,0])
+    for i in range(3):
+        for j in range(3):
+            vector_out[i] += gammaUU[i][j] * fD[j]
+    return vector_out
 
 
 def physical_metric(bar_gamma, phi):
@@ -55,7 +63,7 @@ def physical_metric(bar_gamma, phi):
     return gamma_DD_ij
 
 def trace(A_DD, gamma_DD_ij):
-    '''give my phi and bar_gamma, output the trace of a tensor A'''
+    '''give my physical gamma_DD, output the trace of a tensor A'''
     gamma_UU = np.linalg.inv(gamma_DD_ij)
     out = 0
     for i in [0,1,2]:
